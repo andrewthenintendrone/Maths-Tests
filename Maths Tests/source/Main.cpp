20640da5cc09a706a0dd3f1666dd620346e15a84
Andrew Fitzpatrick <andrewfitzpatrick@y7mail.com>
2017-04-04 19:51:35 +1000

    Realised that Matrix4 determinant function does not work as intended

--- Maths Tests/source/Main.cpp
+++ Maths Tests/source/Main.cpp
@@ -6,11 +6,13 @@
 
 int main()
 {
-	Matrix4 matA(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+	Matrix4 matA(5, 6, 6, 6, 8, 9, 3, 4, 5, 6, 3, 1, 10, 12, 22, 6);
     Matrix4 matB(17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32);
 
-	std::cout << matA << "\n * \n" << matB << "\n = \n" << matA.dot(matB) << std::endl;
-	std::cout << "and it's determinant is " << matA.dot(matB).determinant() << std::endl;
+    std::cout << "The determinant of " << std::endl << matA << std::endl << " is " << matA.determinant() << std::endl;
+
+	//std::cout << matA << "\n * \n" << matB << "\n = \n" << matA.dot(matB) << std::endl;
+	//std::cout << "and it's determinant is " << matA.dot(matB).determinant() << std::endl;
 
 
 	system("pause");
