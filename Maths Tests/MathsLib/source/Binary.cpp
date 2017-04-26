6d191805655765c41208f9b2cc3ea021dd761bf9
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2017-04-26 10:59:43 +1000

    Added function descriptions from the excercise sheet

--- Maths Tests/MathsLib/source/Binary.cpp
+++ Maths Tests/MathsLib/source/Binary.cpp
@@ -2,16 +2,21 @@
 #include <limits>
 #include <iostream>
 
+// Returns true if the left most(the most significant) bit of value is set and false otherwise
 bool IsLeftMostBitSet(unsigned int value)
 {
     return ((UINT_MAX - (UINT_MAX >> 1) & value) > 0);
 }
 
+// Returns true if the right most (the least significant) bit of value is set and false otherwise
 bool IsRightMostBitSet(unsigned int value)
 {
     return ((1 & value) > 0);
 }
 
+// Returns true if the asked for bit is set, and false otherwise.
+// bit_to_check is zero indexed from the right most bit.
+// i.e 0 is the right most bit and 31 is the left most.
 bool IsBitSet(unsigned int value, unsigned char bit_to_check)
 {
     // if the bit to check is more than the number of bits in an unsigned int return false
@@ -25,6 +30,8 @@ bool IsBitSet(unsigned int value, unsigned char bit_to_check)
     }
 }
 
+// This function returns the index of the right most bit set to 1 in value
+// If no bits are set, it returns - 1
 int GetRightMostSetBit(unsigned int value)
 {
     for (unsigned char i = 0; i < sizeof(unsigned int) * CHAR_BIT; i++)
@@ -37,7 +44,7 @@ int GetRightMostSetBit(unsigned int value)
     return -1;
 }
 
-// prints a number converted to binary
+// Prints value to the console as a binary number
 void PrintBinary(unsigned int value)
 {
     std::cout << toBinary(value) << std::endl;
@@ -56,6 +63,7 @@ std::string toBinary(unsigned int value)
     return binaryString;
 }
 
+// Returns true if value is a power of 2 and false otherwise
 bool IsPowerOf2(unsigned int value)
 {
     return true;
