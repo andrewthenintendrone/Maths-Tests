5bff5a4bc94d64d6fed8be3bed0e38703f593e0e
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2017-04-26 10:08:52 +1000

    cpp didnt upload

--- Maths Tests/MathsLib/source/Binary.cpp
+++ Maths Tests/MathsLib/source/Binary.cpp
@@ -1,5 +1,6 @@
 #include "Binary.h"
 #include <limits>
+#include <iostream>
 
 bool IsLeftMostBitSet(unsigned int value)
 {
@@ -21,9 +22,23 @@ int GetRightMostSetBit(unsigned int value)
     return 1;
 }
 
+// prints a number converted to binary
 void PrintBinary(unsigned char value)
 {
+    std::cout << toBinary(value) << std::endl;
+}
 
+// returns a binary version of a number as a string
+std::string toBinary(unsigned int value)
+{
+    unsigned int tempValue = value;
+    std::string binaryString;
+    while (tempValue != 0)
+    {
+        binaryString = (tempValue % 2 == 0 ? "0" : "1") + binaryString;
+        tempValue /= 2;
+    }
+    return binaryString;
 }
 
 bool IsPowerOf2(unsigned int value)
